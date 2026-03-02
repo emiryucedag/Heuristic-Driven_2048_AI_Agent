8c57be5e32a32930ce595932277b36e07edea8db
emiryucedag <emiryucedag@hotmail.com>
2026-03-02 11:48:39 +0300

    check

--- game_2048.py
+++ game_2048.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import numpy as np
 import random
 
-# Tasarım ve Renk Paleti (İnsan benzeri hissetmesi için orijinal renkler)
+
 COLORS = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179", 16: "#f59563", 32: "#f67c5f", 64: "#f65e3b", 
           128: "#edcf72", 256: "#edcc61", 512: "#edc850", 1024: "#edc53f", 2048: "#edc22e"}
 
